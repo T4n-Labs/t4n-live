1b24d01975b268d59f48c82a2996f9c2d0799d93
gh0st4n <darskevoi@gmail.com>
2026-05-01 17:14:14 +0700

    Add: Package Python3-Tkinter for GUI Installer

--- t4n-iso.sh
+++ t4n-iso.sh
@@ -197,7 +197,7 @@ build_variant() {
     SERVICES="sshd chronyd"
     
     XFCE_PKGS="lightdm lightdm-gtk-greeter xfce4 elogind gnome-themes-standard gnome-keyring network-manager-applet gvfs-afc gvfs-mtp gvfs-smb udisks2 firefox xfce4-pulseaudio-plugin"
-    ADD_PKGS="tree bat eza nano vim neovim git curl wget zenity tmux fzf ranger base-devel xdg-utils xtools gparted fastfetch rsync xfce4-screenshooter"
+    ADD_PKGS="tree bat eza nano vim neovim git curl wget zenity tmux fzf ranger base-devel xdg-utils xtools gparted fastfetch rsync xfce4-screenshooter python3-tkinter"
 
     LIGHTDM_SESSION=''
 
